2373dcc547482341d525c8a9ce35529a0fe190f5
NUM-1111 <2943603313@qq.com>
2025-12-01 12:36:10 +0800

    初始化

---
+++ src/main/java/com/intellivault/backend/DocumentAgentApplication.java
@@ -0,0 +1,13 @@
+package com.intellivault.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentAgentApplication.class, args);
+    }
+
+}
